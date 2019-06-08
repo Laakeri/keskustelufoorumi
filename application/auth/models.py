ac309760f915fc739c6c938a6a2d2d0a907acf01
Tuukka Korhonen <tuukka.korhonen@cs.helsinki.fi>
2019-06-09 02:44:59 +0300

    heroku hitler

--- application/auth/models.py
+++ application/auth/models.py
@@ -2,7 +2,7 @@ from application import db
 
 class User(db.Model):
 
-    __tablename__ = "User"
+    __tablename__ = "account"
   
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
--- application/models.py
+++ application/models.py
@@ -2,18 +2,18 @@ from application import db
 
 class Post(db.Model):
 
-    __tablename__ = "Post"
+    __tablename__ = "post"
     
     id = db.Column(db.Integer, primary_key=True)
 
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
     message = db.Column(db.Text, nullable=False)
     
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
-    parent_id = db.Column(db.Integer, db.ForeignKey('Post.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     
-    user = db.relationship('User', foreign_keys=user_id)
-    parent = db.relationship('Post', foreign_keys=parent_id)
+    #user = db.relationship('User', foreign_keys=user_id)
+    #parent = db.relationship('Post', foreign_keys=parent_id)
     
     def __init__(self, message, parent_id, user_id):
         self.message = message
